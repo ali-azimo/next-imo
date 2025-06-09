db198d8a18546577b924408ed0b0835b48721df4
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-09 10:13:01 +0200

    Adicionar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
+       <ClerkProvider>
+           <Header />
+          {children}
+       </ClerkProvider>
       </body>
     </html>
   );
