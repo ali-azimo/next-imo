088725e29da9cda32027c3a795fcdc9f1eb97a49
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-09 18:03:31 +0200

    mo

--- src/libs/mongodb/mongoose.js
+++ src/libs/mongodb/mongoose.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
-
 let inicializar = false;
 
 export const connect = async () =>{
     mongoose.set('strictQuery', true);
-
 
     if(inicializar){
         console.log('MongoDb inicializado');
@@ -14,8 +12,8 @@ export const connect = async () =>{
     try{
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "next-imo",
-            userNewUrlParser: true,
             useUnifiedTopology: true,
+            userNewUrlParser: true,
         });
         inicializar = true;
         console.log("Contacto com MongoDB");
