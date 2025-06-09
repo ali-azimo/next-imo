946ae784d14f6fc5f239dafc9b4b47c8f2022694
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-09 08:47:58 +0200

    homePaage

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-black text-white text-cener">oi pessoal</div>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
