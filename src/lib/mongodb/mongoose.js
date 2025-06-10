dad8130dede50f1842e0fadc5beca7b196abe4fa
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-10 10:22:02 +0200

    atualizaar

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,23 +1,23 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-let inicializar = false;
+// let inicializar = false;
 
-export const connect = async () =>{
-    mongoose.set('strictQuery', true);
+// export const connect = async () =>{
+//     mongoose.set('strictQuery', true);
 
-    if(inicializar){
-        console.log('MongoDb inicializado');
-        return;
-    }
-    try{
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "next-imo",
-            //useUnifiedTopology: true,
-            //userNewUrlParser: true,
-        });
-        inicializar = true;
-        console.log("Contacto com MongoDB");
-    }catch (error){
-        console.log("Erro ao conectar com MongoDB", error)
-    }
-};
+//     if(inicializar){
+//         console.log('MongoDb inicializado');
+//         return;
+//     }
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URI, {
+//             dbName: "next-imo",
+//             useUnifiedTopology: true,
+//             userNewUrlParser: true,
+//         });
+//         inicializar = true;
+//         console.log("Contacto com MongoDB");
+//     }catch (error){
+//         console.log("Erro ao conectar com MongoDB", error)
+//     }
+// };
